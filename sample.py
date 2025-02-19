8bfa6b7b0b64de6ca8d261ac2b8adf9ccd745bbd
Rajeswari-Vatala <vatalarajeswari76@gmail.com>
2025-02-19 22:23:38 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,78 @@
+# # a=0
+# # def avg(c,b):
+# #     total=c+b
+# #     global a
+# #     a+=total
+# #     avg=total/2
+# #     print(avg)
+# # avg(3,4)
+# # print(a)
+# #task4
+# # b=1000
+# # def deposit(amount):
+# #     global b
+# #     b+=amount
+# #     print(b)
+# # def withdraw(amount):
+# #     global b
+# #     if b>amount:
+# #         b-=amount
+# #         print(b)
+# # def checkbal():
+# #     global b
+# #     print(b)
+# # deposit(10)
+# # withdraw(20)
+# # checkbal()
+# #task 5
+# # def area(length,breadth):
+# #     print(length*breadth)
+# # area(10,20)
+# #task 6
+# # def book(title,author,price):
+# #     print(title)
+# #     print(author)
+# #     print(price)
+# # book(title="jaswanth",author="jas",price=100)
+# #task 7
+# # def greet_user(name,message='welcome'):
+# #     print(name)
+# #     print(message)
+# # greet_user("jaswanth")
+# #task
+# bal=1000
+# def deposit(amount):
+#             global bal
+#             bal+=amount
+#             print(bal)
+# def withdraw(amount):
+#             global bal
+#             if bal>amount:
+#                 bal-=amount
+#                 print(bal)
+# def checkbal():
+#             global bal
+#             print(bal)
+# def display():          
+#     a='''
+#     1.deposit
+#     2.withdraw
+#     3.checkbal
+#     '''
+#     print(a)
+#     choose=int(input("choose any option"))
+#     if(choose==1):
+#         amount=int(input("plz enter deposit amount"))
+#         deposit(amount)
+#     elif(choose==2):
+#         b=int(input("enter withdraw amount"))
+#         withdraw(b)
+#     elif(choose==3):
+#         checkbal()
+#     else:
+#         print("please enter valid options")
+
+
+#     display()
+# display()
+    
